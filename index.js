30837ac8022914d556d78931d92291290a10b5bd
Reezus-O <arinzevsvp@gmai.com>
2023-06-06 09:12:31 +0100

    Made the list items show on the page

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 let toDo = document.querySelector(".to-do")
 let add = document.querySelector(".add-button")
+let list = document.querySelector(".to-do-list")
 let toDoList = [];
+let html;
 
 function fillList(item, array) {
     array.push(item)
-    console.log(array)
+    let listHtml = ""; /*Note: this resets the list, so everytime the function is called, the list gets reset and a new list is built from scratch */
+    for (let i = 0; i < array.length; i++) {
+        let content = array[i]
+        html = `<p>${content}</p>`
+        listHtml = listHtml + html
+    }
+    list.innerHTML = listHtml
+    toDo.value = ""
+    console.log(listHtml)
 }
 
 add.addEventListener("click", function () {
